5445fe0537a3613cf3b83065e0311a13d035ad0f
jesssubin <jess.s.moon@gmail.com>
2020-09-28 22:11:01 +0000

    day1 finished

--- express_server.js
+++ express_server.js
@@ -9,9 +9,9 @@ app.set("view engine", "ejs");
 
 
 function generateRandomString() {
-  let randomStr = ""; 
+  let randomStr = "";
   randomStr = Math.random.toString(36).substring(2, 8);
-  return randomStr; 
+  return randomStr;
 }
 
 const urlDatabase = {
@@ -45,9 +45,18 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const shortUrl = generateRandomString(); 
+  urlDatabase[shortUrl] = req.body.longURL;
+  // console.log(shortUrl);  
+  // console.log(urlDatabase); 
+  // console.log(req.body);  // Log the POST request body to the console
+  res.redirect(`/urls/:${shortUrl}`);         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
