b4a626427189589f7d9dd16bb155fd195dc0e40d
jesssubin <jess.s.moon@gmail.com>
2020-09-29 22:39:13 +0000

    login/logout feature added

--- express_server.js
+++ express_server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+const cookieParser = require('cookie-parser');
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cookieParser()); 
 app.set("view engine", "ejs");
 
 
@@ -32,7 +32,10 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase}; 
+  const templateVars = { 
+    username: req.cookies["username"],
+    urls: urlDatabase
+  }; 
   res.render("urls_index", templateVars); 
 })
 
@@ -41,7 +44,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"]};
   res.render("urls_show", templateVars);
 });
 
@@ -59,9 +62,26 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/:${shortUrl}`);         // Respond with 'Ok' (we will replace this)
 });
 
+app.post("/urls/:shortURL", (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  res.redirect(`/urls/${req.params.shortURL}`); 
+  }); 
+
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL]; 
     res.redirect("/urls");
+})
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls"); 
+})
+
+app.post("/logout", (req, res) => {
+  const username  = req.body.username; 
+  res.clearCookie("username", username); 
+  res.redirect("/urls"); 
 })
 
 app.listen(PORT, () => {
