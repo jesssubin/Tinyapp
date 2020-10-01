38015426671f4e6a930e5f3d481886fd1844afb2
jesssubin <jess.s.moon@gmail.com>
2020-10-01 03:30:43 +0000

    anyone can visit short URLs

--- express_server.js
+++ express_server.js
@@ -39,8 +39,8 @@ const users = {
 }
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "userRandomID" },
+  "9sm5xK": { longURL: "http://www.google.com", userID: "user2RandomID" } 
 };
 
 app.use((req, res, next) => {
