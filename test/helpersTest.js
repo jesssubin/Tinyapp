0f87fd21a68f0fb980e44be22047abeaeb52faa3
jesssubin <jess.s.moon@gmail.com>
2020-10-02 01:28:18 +0000

    urlsForUser Test case added

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,6 +1,6 @@
-const { assert } = require('chai');
+const { assert } = require("chai");
 
-const { urlsForUser, getUserByEmail } = require('../helpers.js');
+const { urlsForUser, getUserByEmail } = require("../helpers.js");
 
 const testUsers = {
   "userRandomID": {
@@ -15,16 +15,28 @@ const testUsers = {
   }
 };
 
-describe('getUserByEmail', function () {
-  it('should return a user with valid email', function () {
+const testDatabase = {
+  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "userRandomID" },
+  "9sm5xK": { longURL: "http://www.google.com", userID: "user2RandomID" }
+};
+
+describe("getUserByEmail", function () {
+  it("should return a user with valid email", function () {
     const user = getUserByEmail("user@example.com", testUsers)
     const expectedOutput = "userRandomID";
     assert.equal(user, expectedOutput);
   });
 
-  it('should return undefined if email is invalid', function () {
+  it("should return undefined if email is invalid", function () {
     const user = getUserByEmail("random@example.com", testUsers)
     const expectedOutput = undefined; 
     assert.equal(user, expectedOutput); 
   }); 
 });
+
+describe("urlsForUser", function () {
+  it("should should return user urls with valid user id", function () {
+    const userUrls = urlsForUser("userRandomID", testDatabase); // find all the URLs that belong to the FIRST user in testUsers
+    assert.deepEqual(userUrls, {"b2xVn2": {longURL: "http://www.lighthouselabs.ca", userID: "userRandomID"}});
+  }); 
+}); 
